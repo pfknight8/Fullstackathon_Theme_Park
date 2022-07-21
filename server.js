a392498c9fddc7c0c19fcd1517f18ac895eafc9f
Patrick Knight <pfknight2@gmail.com>
2022-07-21 13:14:11 -0400

    added some initial express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const logger = require('morgan')
+const db = require('./db')
+const routes = require('./routes')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+app.use(express.json)
+app.use(logger('dev'))
+
+app.use('/', routes)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error!'))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
