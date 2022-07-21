a0e5c165151e921aa96b490a2f1cd51b851511b3
Jason Martinez <jasonmar08@gmail.com>
2022-07-21 12:27:04 -0400

    added state to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Review from '../components/Review'
 import RideList from '../components/RideList'
 
-const Home = () => {}
+const Home = () => {
+  const [coasters, setCoasters] = useState([])
+
+  useEffect(() => {
+    // const getCoasters = async () => {
+    //   const res = await axios.get(`#addDataUrlHere`)
+    // }
+  })
+}
