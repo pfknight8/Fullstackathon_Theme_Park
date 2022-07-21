e0355be089ba80ec89219016d21884489b7cabf4
Ryan Saucier <ryanyoubum@gmail.com>
2022-07-21 14:09:30 -0400

    added index and values to ride,user

---
+++ seed/users.js
@@ -0,0 +1,30 @@
+const db = require("../db");
+const Chance = require("chance");
+const { Review, User } = require("../models");
+
+const chance = new Chance();
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+const createUsersWithReviews = async (reviews) => {
+  console.log(reviews);
+  let lenOfItems = 100;
+  const users = [...Array(lenOfItems)].map((user) => {
+    const selectedReviews = reviews.splice(0, reviews.length / lenOfItems);
+    return {
+      first_name: chance.first(),
+      last_name: chance.last(),
+      email: chance.email(),
+      review: selectedReviews.map((review) => review._id),
+    };
+  });
+  await User.insertMany(users);
+  console.log("Created Users with Reviews!");
+};
+
+const run = async () => {
+  await createUsersWithReviews();
+  db.close();
+};
+
+run();
