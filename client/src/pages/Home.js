73a6e63c8c60b1784411f0440e5a1fbdae0f8b24
Jason Martinez <jasonmar08@gmail.com>
2022-07-22 12:33:25 -0400

    code cleanup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-// import ReviewRide from './ReviewRide'
 import RideList from '../components/RideList'
-// import rideSeed from './seed/rides.js'
 
 const Home = ({ handleRideSelect }) => {
   const [rides, setRides] = useState([])
@@ -10,8 +8,6 @@ const Home = ({ handleRideSelect }) => {
   useEffect(() => {
     const getRides = async () => {
       const res = await axios.get(`http://localhost:3001/rides`)
-      // const res = rideSeed
-      console.log(res)
       setRides(res.data.rides)
     }
     getRides()
@@ -28,7 +24,7 @@ const Home = ({ handleRideSelect }) => {
               name={ride.name}
               park={ride.park}
               rating={ride.rating}
-              onClick={() => handleRideSelect(ride)} // change "name" to "_id"
+              onClick={() => handleRideSelect(ride)}
             />
           </div>
         ))}
