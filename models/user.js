b2e660c9173a8b953b62b7ccf463db2ca3e28c04
Ryan Saucier <ryanyoubum@gmail.com>
2022-07-21 20:34:50 -0400

    nested review

--- models/user.js
+++ models/user.js
@@ -5,10 +5,16 @@ const UserSchema = new Schema(
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     email: { type: String, required: true },
-    review: { type: String, required: true }
+    review: [
+      {
+        coasterID: { type: String, required: true },
+        rating: { type: String, required: false },
+        comment: { type: String, required: false },
+      },
+    ],
     //tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }]
   },
   { timestamps: true }
 );
 
-module.exports = UserSchema
+module.exports = UserSchema;
